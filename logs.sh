05d1e5b1abc39cbc4563d577b1df90dd1eb0407b
Pulipuli Chen <pulipuli.chen@gmail.com>
2026-02-15 02:37:54 +0800

    還是沒修好，換人

--- logs.sh
+++ logs.sh
@@ -3,11 +3,11 @@
 # 如果沒有docker compose up 的話，先up起來
 
 # Check if docker compose services are running
-if [ -z "$(docker compose ps --services --filter "status=running")" ]; then
+if [ -z "$(sudo docker compose ps --services --filter "status=running")" ]; then
   echo "Docker Compose services are not running. Starting them up..."
-  docker compose up -d
+  sudo docker compose up -d
 else
   echo "Docker Compose services are already running."
 fi
 
-docker compose logs -f
+sudo docker compose logs -f
